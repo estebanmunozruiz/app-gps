a81e8a537b71b2d1ff1221c41c5495d5100c82f3
estebanmunozruiz <estebanmunozruiz11@gmail.com>
2026-05-29 20:17:30 -0400

    Delete aplicacion gps directory

--- aplicacion gps/abrir-app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-echo Iniciando GPS Agricola...
-echo.
-start "" http://127.0.0.1:5173/index.html
-"C:\Users\cuent\.cache\codex-runtimes\codex-primary-runtime\dependencies\python\python.exe" -m http.server 5173 --bind 0.0.0.0
-echo.
-echo El servidor se cerro. Presiona una tecla para salir.
-pause > nul
